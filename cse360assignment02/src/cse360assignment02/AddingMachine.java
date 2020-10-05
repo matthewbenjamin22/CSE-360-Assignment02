39dadf8a1b42d409712e027012a4639e57d8ac77
Matthew Benjamin <mbenjam5@asu.edu>
2020-10-04 17:11:07 -0700

    Initial Version - Complete with comments

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,43 +1,54 @@
+/**
+ * AddingMachine.java
+ * Created for CSE 360 Assignment 1 (Referred to as assignment02 by code despite being assignment 1)
+ * @Author Matthew Benjamin
+ * Student ID 1214379491
+ */
+
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
   
   /**
- * 
+ *	Constructor 
  */
 public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   /**
- * @return
+ * Accessor method
+ * @return total
  */
 public int getTotal () {
     return 0;
   }
   
   /**
+ * Add method
  * @param value
  */
 public void add (int value) {
   }
 
   /**
+ * Subtract Method
  * @param value
  */
 public void subtract (int value) {
   }
 
-  /* (non-Javadoc)
- * @see java.lang.Object#toString()
+/**
+ * toString Method
+ * @return the String result
  */
 public String toString () {
     return "";
   }
 
   /**
- * 
+ * Clear method
  */
 public void clear() {
   }
