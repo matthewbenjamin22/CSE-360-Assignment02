2e011db49cd7a62b432195562c6c5afcf6c56264
Matthew Benjamin <mbenjam5@asu.edu>
2020-10-04 17:03:37 -0700

    Initial Version for CSE360Assignment02

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,45 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  /**
+ * 
+ */
+public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+ * @return
+ */
+public int getTotal () {
+    return 0;
+  }
+  
+  /**
+ * @param value
+ */
+public void add (int value) {
+  }
+
+  /**
+ * @param value
+ */
+public void subtract (int value) {
+  }
+
+  /* (non-Javadoc)
+ * @see java.lang.Object#toString()
+ */
+public String toString () {
+    return "";
+  }
+
+  /**
+ * 
+ */
+public void clear() {
+  }
+   
+}
